a71bb7acf8a8c40a0b676b72e0ed480fa75dfb8e
wildan <wildanakhmad3584@gmail.com>
2024-12-12 21:49:09 +0700

    manajemen stok

--- main.cpp
+++ main.cpp
@@ -3,36 +3,55 @@
 using namespace std;
 
 int app = 1, role, menu, aksi;
-const int size_suplier = 10;
+const int size_suplier = 100;
+const int size_stoks = 100;
+// const int size_stok_historis = 1000;
 
 
 // data suplier
 int current_position_suplier = 0;
 string kodes[size_suplier];
+string namas[size_suplier];
 string bahans[size_suplier];
-int jumlahs[size_suplier];
 // data suplier
 
+// data stok
+int current_position_stok = 0;
+string kode_stoks[size_stoks];
+string nama_stoks[size_stoks];
+int jumlah_stoks[size_stoks];
+// data stok
+
+// // data stok histori
+// int current_position_stok_histori = 0;
+// string kode_stok_historis[size_stok_historis];
+// string nama_stok_historis[size_stok_historis];
+// int jumlah_stok_historis[size_stok_historis];
+
+// // data stok histori
+
 void tambahSuplier() {
-    string kode, bahan;
-    int jumlah;
+    string kode,nama, bahan;
+   
     cout << "\nMasukan kode: ";
     cin >> kode;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Masukan nama suplier: ";
+    getline(cin, nama);
     cout << "Masukan jenis bahan: ";
     getline(cin, bahan);
-    cout << "Masukan jumlah: ";
-    cin >> jumlah;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    // cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
 
     if(current_position_suplier > size_suplier) {
 
     } else {
         kodes[current_position_suplier] = kode;
+        namas[current_position_suplier] = nama;
         bahans[current_position_suplier] = bahan;
-        jumlahs[current_position_suplier] = jumlah;
         current_position_suplier++;
+
+        cout << "\nHoreyy suplier " << nama << " berhasil di tambah...." << endl;
     }
 
     // printSuplier();
@@ -40,8 +59,8 @@ void tambahSuplier() {
 
 void printSuplier() {
     cout <<""<<endl;
-    cout << "Kode                Jenis bahan        Jumlah" << endl;
-    cout << "---------------------------------------------" << endl;
+    cout << "Kode                Suplier             Jenis bahan" << endl;
+    cout << "---------------------------------------------------" << endl;
 
 
     for (int i = 0; i < current_position_suplier; i++)
@@ -54,18 +73,103 @@ void printSuplier() {
             cout<<" ";
         }
         
-        cout << bahans[i];
-         for (int j = bahans[i].length(); j < 20; j++)
+        cout << namas[i];
+         for (int j = namas[i].length(); j < 20; j++)
         {
             /* code */
             cout<<" ";
         }
-        cout << jumlahs[i] << endl;
+        cout << bahans[i] << endl;
 
     }
     
 
 }
+
+int indexOfStock(string nama, string kode) {
+    for (int i = 0; i <= current_position_stok; i++)
+    {
+        /* code */
+
+        if(nama_stoks[i] == nama && kode_stoks[i] == kode) {
+            return i;
+        }
+
+    }
+
+    return -1;
+    
+}
+
+
+void printBahanBaku() {
+    cout <<""<<endl;
+    cout << "Kode                Jenis bahan        Jumlah" << endl;
+    cout << "---------------------------------------------" << endl;
+
+
+    for (int i = 0; i < current_position_stok; i++)
+    {
+        /* code */
+        cout << kode_stoks[i];
+        for (int j = kode_stoks[i].length(); j < 20; j++)
+        {
+            /* code */
+            cout<<" ";
+        }
+        
+        cout << nama_stoks[i];
+         for (int j = nama_stoks[i].length(); j < 20; j++)
+        {
+            /* code */
+            cout<<" ";
+        }
+        cout << jumlah_stoks[i] << endl;
+
+    }
+    
+
+}
+
+void tambahBahanBaku() {
+    string kode,bahan;
+    int jumlah;
+
+    cout << "\nMasukan kode: ";
+    cin >> kode;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Masukan jenis bahan: ";
+    getline(cin, bahan);
+    cout << "Masukan jumlah: ";
+    cin >> jumlah;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int x = indexOfStock(bahan, kode);
+    if(x != -1) {
+        jumlah_stoks[x]+= jumlah;
+        cout << "\nHoreyy stock "<< bahan << " berhasil di perbaharui...." << endl;
+    } else if(x == -1 && size_stoks > current_position_stok){
+        kode_stoks[current_position_stok] = kode;
+        nama_stoks[current_position_stok] = bahan;
+        jumlah_stoks[current_position_stok] = jumlah;
+
+        current_position_stok++;
+
+        cout << "\nHoreyy stock "<< bahan << " berhasil di ditambah...." << endl;
+    }
+}
+
+// tambah stok bisa pilih dari daftar bahan di suplier atau langsung
+// sebelum itu ada pilihan terlebih dahulu apakah mau ambil dari daftar
+// jika iya akan tampil bahan beserta nomernya
+// dan user bisa input nomer dari bahan tsb
+// setelah pilih maka tinggal input kode dan jumlah
+// jika tidak input manual semuanya
+// sterlah input proses validasi jika bahan sudah tersedia di stok 
+// maka increase stok
+// jika belum ada buat baru
+
+
 
 void pilihRole()
 {
@@ -107,7 +211,7 @@ void aksiMenuSplier()
     int aksi;
     do
     {
-        cout << "\n Sub Menu Suplier "
+        cout << "\n Manajemen Suplier "
              << "\n1. Tambah data"
              << "\n2. Data suplier"
              << "\n3. Kembali" << endl;
@@ -119,11 +223,9 @@ void aksiMenuSplier()
         switch (aksi)
         {
         case 1:
-            // cout << "\nAksi tambah suplier" << endl;
             tambahSuplier();
             break;
         case 2:
-            // cout << "\nAksi melihat data suplier" << endl;
             printSuplier();
             break;
 
@@ -140,14 +242,39 @@ void aksiMenuSplier()
 
 void aksiMenuBahanBaku()
 {
-    cout << "\n Sub Menu Bahan Baku "
+    
+
+    int aksi;
+
+    do
+    {
+        cout << "\n Manajemen Bahan Baku "
          << "\n1. Tambah data"
          << "\n2. Riwayat stok"
-         << "\n3. Riwayat penggunaan"
-         << "\n4. Peringatan stok"
-         << "\n5. Kembali" << endl;
+         << "\n3. Kembali" << endl;
 
-    cout << "Pilih aksi: ";
+        cout << "Pilih aksi: ";
+
+        cin >> aksi;
+
+        switch (aksi)
+        {
+        case 1:
+            tambahBahanBaku();
+            break;
+        case 2:
+            printBahanBaku();
+            break;
+     
+        case 3:
+            cout << "Kembali ke pilih menu admin!" << endl;
+            break;
+
+        default:
+            cout << "Pilihan tidak valid silakan pilih aksi kembali!" << endl;
+        }
+
+    } while (aksi != 3);
 }
 
 void aksiMenuProyek()
@@ -208,34 +335,7 @@ int main()
                     break;
                 case 2:
 
-                    do
-                    {
-                        aksiMenuBahanBaku();
-                        cin >> aksi;
-
-                        switch (aksi)
-                        {
-                        case 1:
-                            cout << "Aksi tambah bahan baku" << endl;
-                            break;
-                        case 2:
-                            cout << "Aksi melihat riwayat stok" << endl;
-                            break;
-                        case 3:
-                            cout << "Aksi melihat riwayat penggunaan" << endl;
-                            break;
-                        case 4:
-                            cout << "Aksi peringatan stok" << endl;
-                            break;
-                        case 5:
-                            cout << "Kembali ke pilih menu admin!" << endl;
-                            break;
-
-                        default:
-                            cout << "Pilihan tidak valid silakan pilih aksi kembali!" << endl;
-                        }
-
-                    } while (aksi != 5);
+                    aksiMenuBahanBaku();
                     break;
                 case 3:
 
